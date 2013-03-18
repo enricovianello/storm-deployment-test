d5668463af0252be0a3df1d148fa4ae2a1695e3d
Enrico Vianello <maverick17586@gmail.com>
2013-03-18 11:24:06 +0100

    Update emi-storm-uninstall.sh

--- emi-storm-uninstall.sh
+++ emi-storm-uninstall.sh
@@ -1,9 +1,23 @@
-yum erase -y storm-xmlrpc-c storm-xmlrpc-c-client emi-storm-gridhttps-mp storm-dynamic-info-provider storm-globus-gridftp-server yaim-storm storm-gridhttps-server emi-storm-frontend-mp storm-backend-server emi-storm-backend-mp storm-frontend-server emi-storm-globus-gridftp-mp storm-gridhttps-plugin storm-pre-assembled-configuration
-userdel gridhttps
-groupdel gridhttps
-userdel storm
-groupdel storm
-rm -rf /var/log/storm
-rm -rf /etc/storm
-yum erase -y emi-release
-yum erase -y epel-release
+# StoRM uninstall script
+
+execute() {
+  echo "[root@`hostname` ~]# $1"
+  eval "$1"
+  exit_status=$?
+  if [ $exit_status -ne 0 ]; then
+  echo "Deployment failed"; 
+	kill -s TERM $TOP_PID
+  fi
+}
+
+echo "StoRM 1.11 uninstall.."
+
+execute "yum erase -y storm-xmlrpc-c storm-xmlrpc-c-client emi-storm-gridhttps-mp storm-dynamic-info-provider storm-globus-gridftp-server yaim-storm storm-gridhttps-server emi-storm-frontend-mp storm-backend-server emi-storm-backend-mp storm-frontend-server emi-storm-globus-gridftp-mp storm-gridhttps-plugin storm-pre-assembled-configuration"
+execute "userdel gridhttps"
+execute "groupdel gridhttps"
+execute "userdel storm"
+execute "groupdel storm"
+execute "rm -rf /var/log/storm"
+execute "rm -rf /etc/storm"
+execute "yum erase -y emi-release"
+execute "yum erase -y epel-release"
