523d75b7455572f5588f86656af51eee21017658
Enrico Vianello <maverick17586@gmail.com>
2013-05-29 16:11:06 +0300

    Update configure-environment-vars.sh

--- testsuite-deployment/configure-environment-vars.sh
+++ testsuite-deployment/configure-environment-vars.sh
@@ -3,7 +3,7 @@
 userid=$(id -u $(whoami))
 export X509_USER_PROXY=/tmp/x509up_u$userid
 export SRM_PATH=/opt/d-cache/srm
-export PATH=$PATH:/opt/d-cache/srm
+export PATH=$PATH:/opt/d-cache/srm/bin
 
 echo "X509_USER_PROXY=$X509_USER_PROXY"
 echo "SRM_PATH=$SRM_PATH"
