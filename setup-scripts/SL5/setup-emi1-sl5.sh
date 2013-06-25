b08dfc1f708dd96d60b250b056cda1baea7b5c0c
Enrico Vianello <maverick17586@gmail.com>
2013-06-25 18:10:51 +0300

    Create setup-emi1-sl5.sh

---
+++ setup-scripts/SL5/setup-emi1-sl5.sh
@@ -0,0 +1,2 @@
+export EMI_RELEASE="http://emisoft.web.cern.ch/emisoft/dist/EMI/1/sl5/x86_64/updates/emi-release-1.0.1-1.sl5.noarch.rpm"
+export EPEL_RELEASE="http://archives.fedoraproject.org/pub/epel/5/x86_64/epel-release-5-4.noarch.rpm"
