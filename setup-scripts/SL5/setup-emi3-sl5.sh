92c4031da12182525288b6aefd319e80d8c5aea2
Enrico Vianello <maverick17586@gmail.com>
2013-06-27 15:22:45 +0200

    Update setup-emi3-sl5.sh

--- setup-scripts/SL5/setup-emi3-sl5.sh
+++ setup-scripts/SL5/setup-emi3-sl5.sh
@@ -1,3 +1,4 @@
 export EMI_RELEASE_REMOTE_RPM="http://emisoft.web.cern.ch/emisoft/dist/EMI/3/sl5/x86_64/base/emi-release-3.0.0-2.el5.noarch.rpm"
 export EPEL_RELEASE_REMOTE_RPM="http://archives.fedoraproject.org/pub/epel/5/x86_64/epel-release-5-4.noarch.rpm"
 export JAVA_LOCATION="/usr/lib/jvm/java-1.6.0-openjdk-1.6.0.0.x86_64/jre"
+export CREATE_GRIDHTTPS_USER="true"
