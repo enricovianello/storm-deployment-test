97818caa5da223466d8a602f4e3c7a45c4d36566
Enrico Vianello <maverick17586@gmail.com>
2013-06-27 15:14:29 +0200

    Update setup-emi2-sl5.sh

--- setup-scripts/SL5/setup-emi2-sl5.sh
+++ setup-scripts/SL5/setup-emi2-sl5.sh
@@ -1,2 +1,3 @@
 export EMI_RELEASE_REMOTE_RPM="http://emisoft.web.cern.ch/emisoft/dist/EMI/2/sl5/x86_64/base/emi-release-2.0.0-1.sl5.noarch.rpm"
 export EPEL_RELEASE_REMOTE_RPM="http://archives.fedoraproject.org/pub/epel/5/x86_64/epel-release-5-4.noarch.rpm"
+export YAIM_CONFIGURATION_FILE="https://raw.github.com/italiangrid/storm-deployment-test/master/siteinfo/emi2-storm.def"
