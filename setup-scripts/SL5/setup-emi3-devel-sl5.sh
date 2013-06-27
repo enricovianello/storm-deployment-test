2a5d09945fb5d3de562463e214cab2a392a0f392
Enrico Vianello <maverick17586@gmail.com>
2013-06-27 15:22:32 +0200

    Update setup-emi3-devel-sl5.sh

--- setup-scripts/SL5/setup-emi3-devel-sl5.sh
+++ setup-scripts/SL5/setup-emi3-devel-sl5.sh
@@ -2,3 +2,4 @@ export ADDITIONAL_REPO="http://radiohead.cnaf.infn.it:9999/view/STORM/job/storm-
 export EMI_RELEASE_REMOTE_RPM="http://emisoft.web.cern.ch/emisoft/dist/EMI/3/sl5/x86_64/base/emi-release-3.0.0-2.el5.noarch.rpm"
 export EPEL_RELEASE_REMOTE_RPM="http://archives.fedoraproject.org/pub/epel/5/x86_64/epel-release-5-4.noarch.rpm"
 export JAVA_LOCATION="/usr/lib/jvm/java-1.6.0-openjdk-1.6.0.0.x86_64/jre"
+export CREATE_GRIDHTTPS_USER="true"
