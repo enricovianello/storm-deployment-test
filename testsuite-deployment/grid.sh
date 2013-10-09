a65836209afc108a6c07a47850aac6caa360bf0f
Enrico Vianello <maverick17586@gmail.com>
2013-10-09 16:57:37 +0200

    Update grid.sh

--- testsuite-deployment/grid.sh
+++ testsuite-deployment/grid.sh
@@ -5,3 +5,4 @@ export X509_USER_PROXY=/tmp/x509up_u$userid
 export JAVA_HOME="/usr/bin/java"
 export SRM_PATH=/opt/d-cache/srm
 export PATH=$PATH:/opt/d-cache/srm/bin
+export LD_LIBRARY_PATH=/opt/d-cache/dcap/lib64
