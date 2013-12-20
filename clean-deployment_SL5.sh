001adb005b602256ccb2b82c7ac3c8824abf3eb7
Valerio Venturi <valerio.venturi@cnaf.infn.it>
2013-12-20 15:16:26 +0100

    Use env variable for storm repo

--- clean-deployment_SL5.sh
+++ clean-deployment_SL5.sh
@@ -5,12 +5,15 @@
 trap "exit 1" TERM
 set -ex
 
+# use the STORM_REPO env variable for the repo, or default to the develop repo
+STORM_REPO=${STORM_REPO:-http://radiohead.cnaf.infn.it:9999/view/REPOS/job/repo_storm_develop_SL5/lastSuccessfulBuild/artifact/storm_develop_sl5.repo}
+
 # install emi-release
 wget http://emisoft.web.cern.ch/emisoft/dist/EMI/3/sl5/x86_64/base/emi-release-3.0.0-2.el5.noarch.rpm
 yum localinstall --nogpgcheck -y emi-release-3.0.0-2.el5.noarch.rpm
 
 # install the storm repo
-wget http://radiohead.cnaf.infn.it:9999/view/REPOS/job/repo_storm_develop_SL5/lastSuccessfulBuild/artifact/storm_develop_sl5.repo -O /etc/yum.repos.d/storm_develop_sl5.repo
+wget $STORM_REPO -O /etc/yum.repos.d/storm.repo
 
 # install
 yum clean all
--- update-deployment_SL5.sh
+++ update-deployment_SL5.sh
@@ -5,6 +5,9 @@
 trap "exit 1" TERM
 set -ex
 
+# use the STORM_REPO env variable for the repo, or default to the develop repo
+STORM_REPO=${STORM_REPO:-http://radiohead.cnaf.infn.it:9999/view/REPOS/job/repo_storm_develop_SL5/lastSuccessfulBuild/artifact/storm_develop_sl5.repo}
+
 # install emi-release
 wget http://emisoft.web.cern.ch/emisoft/dist/EMI/3/sl5/x86_64/base/emi-release-3.0.0-2.el5.noarch.rpm
 yum localinstall --nogpgcheck -y emi-release-3.0.0-2.el5.noarch.rpm
@@ -30,7 +33,7 @@ wget https://raw.github.com/italiangrid/storm-deployment-test/master/siteinfo/st
 /opt/glite/yaim/bin/yaim -c -d 6 -s /etc/storm/siteinfo/storm.def -n se_storm_backend -n se_storm_frontend -n se_storm_gridftp -n se_storm_gridhttps
 
 # install the storm repo
-wget http://radiohead.cnaf.infn.it:9999/view/REPOS/job/repo_storm_develop_SL5/lastSuccessfulBuild/artifact/storm_develop_sl5.repo -O /etc/yum.repos.d/storm_develop_sl5.repo
+wget $STORM_REPO -O /etc/yum.repos.d/storm.repo
 
 # update
 yum clean all
