f6378b1d759cd377ccf3660e6b91a2f72860b64d
Enrico Vianello <maverick17586@gmail.com>
2014-07-22 14:54:17 +0200

    Update clean-deployment_SL6.sh
    Removed gridhttps user creation
    Added the availability to specify a site configuration file
    Added vo dteam

--- clean-deployment_SL6.sh
+++ clean-deployment_SL6.sh
@@ -10,6 +10,9 @@ set -ex
 # use the STORM_REPO env variable for the repo, or default to the develop repo
 STORM_REPO=${STORM_REPO:-http://radiohead.cnaf.infn.it:9999/view/REPOS/job/repo_storm_develop_SL6/lastSuccessfulBuild/artifact/storm_develop_sl6.repo}
 
+# use the STORM_DEF env variable for the site configuration file, or default
+STORM_DEF=${STORM_DEF:-https://raw.github.com/italiangrid/storm-deployment-test/master/siteinfo/storm.def}
+
 # install emi-release
 wget $WGET_OPTIONS http://emisoft.web.cern.ch/emisoft/dist/EMI/3/sl6/x86_64/base/emi-release-3.0.0-2.el6.noarch.rpm
 yum localinstall --nogpgcheck -y emi-release-3.0.0-2.el6.noarch.rpm
@@ -23,13 +26,12 @@ yum install -y emi-storm-backend-mp emi-storm-frontend-mp emi-storm-globus-gridf
 
 # add some users
 adduser -r storm
-adduser -r gridhttps
-usermod -a -G storm gridhttps
 
 # download siteinfo file
 mkdir -p /etc/storm/siteinfo/vo.d
-wget $WGET_OPTIONS https://raw.github.com/italiangrid/storm-deployment-test/master/siteinfo/storm.def -O /etc/storm/siteinfo/storm.def
+wget $WGET_OPTIONS $STORM_DEF -O /etc/storm/siteinfo/storm.def
 wget $WGET_OPTIONS https://raw.github.com/italiangrid/storm-deployment-test/master/siteinfo/vo.d/testers.eu-emi.eu -O /etc/storm/siteinfo/vo.d/testers.eu-emi.eu
+wget $WGET_OPTIONS https://raw.github.com/italiangrid/storm-deployment-test/master/siteinfo/vo.d/dteam -O /etc/storm/siteinfo/vo.d/dteam
 wget $WGET_OPTIONS https://raw.github.com/italiangrid/storm-deployment-test/master/siteinfo/storm-users.conf -O /etc/storm/siteinfo/storm-users.conf
 wget $WGET_OPTIONS https://raw.github.com/italiangrid/storm-deployment-test/master/siteinfo/storm-groups.conf -O /etc/storm/siteinfo/storm-groups.conf
 wget $WGET_OPTIONS https://raw.github.com/italiangrid/storm-deployment-test/master/siteinfo/storm-wn-list.conf -O /etc/storm/siteinfo/storm-wn-list.conf
