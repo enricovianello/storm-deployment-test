a430f4de31e6d66e5f0582df3a4df3bb0e38385f
dandreotti <daniele.andreotti@cnaf.infn.it>
2015-02-24 15:23:35 +0100

    Added fix for grants

--- post-config-setup.sh
+++ post-config-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -ex
+set -x
 trap "exit 1" TERM
 
 # add SAs links
@@ -9,4 +9,12 @@ ln -s ../noauth noauth_sa
 
 cd /storage/noauth/
 ln -s ../testers.eu-emi.eu testers
+
+# fix db grants for root@'fqdn' if needed
+HOSTNAME=`hostname -f`
+
+mysql -h ${HOSTNAME} -u root -p${STORM_DB_PASSWORD} -e"use storm_db;" > /dev/null 2> /dev/null
+if [ "$?" -ne 0 ]; then
+    mysql -u root -p${STORM_DB_PASSWORD} -e"GRANT ALL PRIVILEGES ON *.* TO 'root'@'${HOSTNAME}' IDENTIFIED BY '${STORM_DB_PASSWORD}' WITH GRANT OPTION; FLUSH PRIVILEGES" > /dev/null 2> /dev/null
+fi
 
