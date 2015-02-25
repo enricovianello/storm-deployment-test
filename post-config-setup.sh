c32ee9055bf47b66f3f48a084dd227990d5dbce2
dandreotti <daniele.andreotti@cnaf.infn.it>
2015-02-25 14:17:31 +0100

    Fixed procedure for grants management

--- post-config-setup.sh
+++ post-config-setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-set -x
+set -ex
 trap "exit 1" TERM
+
+pushd .
 
 # add SAs links
 cd /storage/testers.eu-emi.eu/
@@ -13,8 +15,9 @@ ln -s ../testers.eu-emi.eu testers
 # fix db grants for root@'fqdn' if needed
 HOSTNAME=`hostname -f`
 
-mysql -h ${HOSTNAME} -u root -p${STORM_DB_PASSWORD} -e"use storm_db;" > /dev/null 2> /dev/null
-if [ "$?" -ne 0 ]; then
-    mysql -u root -p${STORM_DB_PASSWORD} -e"GRANT ALL PRIVILEGES ON *.* TO 'root'@'${HOSTNAME}' IDENTIFIED BY '${STORM_DB_PASSWORD}' WITH GRANT OPTION; FLUSH PRIVILEGES" > /dev/null 2> /dev/null
-fi
+popd
+source ./siteinfo/storm.def
+
+mysql -u root -p${MYSQL_PASSWORD} -e"GRANT ALL PRIVILEGES ON *.* TO 'root'@'${HOSTNAME}' IDENTIFIED BY '${MYSQL_PASSWORD}' WITH GRANT OPTION; FLUSH PRIVILEGES" > /dev/null 2> /dev/null
+
 
