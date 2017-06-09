91eb42accd875fbe253ebe22d1d85f088b7cc302
enricovianello <maverick17586@gmail.com>
2017-06-09 17:31:48 +0200

    Removed unused stuff after update

--- common/post-update.sh
+++ common/post-update.sh
@@ -2,4 +2,6 @@
 set -ex
 trap "exit 1" TERM
 
-# nothing to do
+# Removed no more used stuff
+yum remove -y storm-gridhttps-plugin
+yum remove -y java-1.6.0-openjdk java-1.7.0-openjdk java-1.7.0-openjdk-devel
