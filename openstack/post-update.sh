6c3bdf11f8c50175033243baff0cb12c886101a2
enricovianello <maverick17586@gmail.com>
2017-06-09 12:04:42 +0200

    namespace update is still on another branch

--- openstack/post-update.sh
+++ openstack/post-update.sh
@@ -4,9 +4,3 @@ trap "exit 1" TERM
 
 yum remove -y storm-gridhttps-plugin
 yum remove -y java-1.6.0-openjdk java-1.7.0-openjdk java-1.7.0-openjdk-devel
-
-# update namespace schema
-
-cd /etc/storm/backend-server
-mv namespace-1.5.0.xsd namespace-1.5.0.xsd.old
-mv namespace-1.5.0.xsd.rpmnew namespace-1.5.0.xsd
