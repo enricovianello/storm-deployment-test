f31f99e720e88d5fce40b2ba6e8907cc2375f38d
enricovianello <maverick17586@gmail.com>
2017-06-12 14:54:28 +0200

    Added deployment script for cdmi
    Used a similar strategy storm deployment like

---
+++ docker/clean-cdmi-deployment_centos6.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -ex
+trap "exit 1" TERM
+
+COMMON_PATH="../common"
+APPLICATION_CONFIG_PATH="/var/lib/cdmi-server/config"
+PLUGINS_CONFIG_PATH="/etc/cdmi-server/plugins"
+REDIS_HOSTNAME="redis.cnaf.infn.it"
+
+source ${COMMON_PATH}/input.env
+
+# install cdmi_storm
+wget $STORM_REPO -O /etc/yum.repos.d/cdmi-storm.repo
+yum clean all
+yum install -y cdmi-storm
+
+# Configure
+rm -rf ${APPLICATION_CONFIG_PATH}/application.yml
+cp -rf ../cdmi/application.yml ${APPLICATION_CONFIG_PATH}/application.yml
+sed -i 's/CLIENT_ID/${CLIENT_ID}/g' ${APPLICATION_CONFIG_PATH}/application.yml
+sed -i 's/CLIENT_SECRET/${CLIENT_SECRET}/g' ${APPLICATION_CONFIG_PATH}/application.yml
+
+mkdir -p ${PLUGINS_CONFIG_PATH}
+cp -rf ../cdmi/capabilities ${PLUGINS_CONFIG_PATH}
+cp -rf ../cdmi/storm-properties.json ${PLUGINS_CONFIG_PATH}/storm-properties.json
+
+# Wait for redis server
+MAX_RETRIES=600
+attempts=1
+CMD="nc -w1 ${REDIS_HOSTNAME} 6379"
+
+echo "Waiting for Redis server ... "
+$CMD
+
+while [ $? -eq 1 ] && [ $attempts -le  $MAX_RETRIES ];
+do
+  sleep 5
+  let attempts=attempts+1
+  $CMD
+done
+
+if [ $attempts -gt $MAX_RETRIES ]; then
+    echo "Timeout!"
+    exit 1
+fi
+
+cd /var/lib/cdmi-server
+su - cdmi -c "./cdmi-server-1.2.jar"
