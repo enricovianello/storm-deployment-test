31672e50d56d35b40935ddf34d1b99def0908c6d
enricovianello <maverick17586@gmail.com>
2017-07-03 14:49:58 +0200

    More priorities to StoRM Repo instead of UMD

--- common/install-umd-repos.sh
+++ common/install-umd-repos.sh
@@ -8,5 +8,8 @@ rpm --import http://repository.egi.eu/sw/production/umd/UMD-RPM-PGP-KEY
 wget http://repository.egi.eu/sw/production/umd/3/repofiles/sl6/UMD-3-base.repo -O /etc/yum.repos.d/UMD-3-base.repo
 wget http://repository.egi.eu/sw/production/umd/3/repofiles/sl6/UMD-3-updates.repo -O /etc/yum.repos.d/UMD-3-updates.repo
 
+# We want to give more priority to the StoRM Repository than UMD
+sed -i "s/priority=1/priority=2/" /etc/yum.repos.d/UMD-3-base.repo /etc/yum.repos.d/UMD-3-updates.repo
+
 # clean
 yum clean all
