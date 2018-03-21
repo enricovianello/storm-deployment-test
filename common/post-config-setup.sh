3f8b1d28b677198e6a2796eff14184e85ab7831c
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2018-03-21 15:53:18 +0100

    allow root access from any host

--- common/post-config-setup.sh
+++ common/post-config-setup.sh
@@ -34,5 +34,4 @@ HOSTNAME=`hostname -f`
 popd
 source ../siteinfo/${VERSION}/storm.def
 
-mysql -u root -p${MYSQL_PASSWORD} -e"GRANT ALL PRIVILEGES ON *.* TO 'root'@'${HOSTNAME}' IDENTIFIED BY '${MYSQL_PASSWORD}' WITH GRANT OPTION; FLUSH PRIVILEGES" > /dev/null 2> /dev/null
-
+mysql -u root -p${MYSQL_PASSWORD} -e"GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}' WITH GRANT OPTION; FLUSH PRIVILEGES" > /dev/null 2> /dev/null
